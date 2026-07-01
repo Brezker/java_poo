e44bb2e5e6085f623a346de5e21e15a99849a444
Brezker <brezker10@gmail.com>
2026-07-01 16:13:20 -0600

    Uso de switch

--- src/main/java/org/brezker/Main.java
+++ src/main/java/org/brezker/Main.java
@@ -1,30 +1,10 @@
 package org.brezker;
-import java.awt.desktop.SystemEventListener;
-import java.util.Scanner;
 public class Main {
     public static void main(String[] args) {
-        Scanner scanner = new Scanner(System.in);
-        System.out.println("Ingrese un numero del 1 al 5: ");
-        int number = scanner.nextInt();
-        switch (number){
-            case 1:
-                System.out.println("Hoy es lunes");
-                break;
-            case 2:
-                System.out.println("Hoy es martes");
-                break;
-            case 3:
-                System.out.println("Hoy es miercoles");
-                break;
-            case 4:
-                System.out.println("Hoy es jueves");
-                break;
-            case 5:
-                System.out.println("Hoy es viernes");
-                break;
-            default:
-                System.out.println("Opcion invalida");
-                break;
+        int counter = 1;
+        while (counter<=5){
+            System.out.println("Numero "+counter);
+            counter++;
         }
     }
 }
