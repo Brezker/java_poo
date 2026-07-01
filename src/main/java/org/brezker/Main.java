d9893a006344fefb38f5eabdbb759850d9277232
Brezker <brezker10@gmail.com>
2026-07-01 16:20:21 -0600

    Uso de for

--- src/main/java/org/brezker/Main.java
+++ src/main/java/org/brezker/Main.java
@@ -1,10 +1,9 @@
 package org.brezker;
 public class Main {
     public static void main(String[] args) {
-        int counter = 1;
-        while (counter<=5){
-            System.out.println("Numero "+counter);
-            counter++;
+//        int counter = 1;
+        for (int i=1;i<=5;i++){
+            System.out.println("Numero "+i);
         }
     }
 }
