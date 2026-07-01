ab89cd7d80f441e2f96e68bbc0e1e5cb8a8a0bae
Brezker <brezker10@gmail.com>
2026-07-01 15:45:18 -0600

    Uso de switch

--- src/main/java/org/brezker/Main.java
+++ src/main/java/org/brezker/Main.java
@@ -4,15 +4,27 @@ import java.util.Scanner;
 public class Main {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
-        System.out.println("Ingrese su edad: ");
-        int edad = scanner.nextInt();
-        System.out.println("Tu edad es: "+edad);
-        if (edad>51){
-            System.out.println("Eres adulto mayor");
-        } else if (edad>17){
-            System.out.println("Eres mayor de edad");
-        } else {
-            System.out.println("Eres menor de edad");
+        System.out.println("Ingrese un numero del 1 al 5: ");
+        int number = scanner.nextInt();
+        switch (number){
+            case 1:
+                System.out.println("Hoy es lunes");
+                break;
+            case 2:
+                System.out.println("Hoy es martes");
+                break;
+            case 3:
+                System.out.println("Hoy es miercoles");
+                break;
+            case 4:
+                System.out.println("Hoy es jueves");
+                break;
+            case 5:
+                System.out.println("Hoy es viernes");
+                break;
+            default:
+                System.out.println("Opcion invalida");
+                break;
         }
     }
 }
