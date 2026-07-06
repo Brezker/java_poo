2eba75740a2e974471390cc5d2fd197ca585f4e2
Brezker <brezker10@gmail.com>
2026-07-05 21:43:23 -0600

    Metodos full

--- src/main/java/org/brezker/Main.java
+++ src/main/java/org/brezker/Main.java
@@ -1,11 +1,44 @@
 package org.brezker;
+
+import java.util.Scanner;
+
 public class Main {
+    public static void saludar(){
+        System.out.println("Hola");
+    }
     public static int sumar(int numero1, int numero2){
-        int resultado=numero1+numero2;
-        return resultado;
+        return numero1+numero2;
+    }
+    public static boolean esPar(int num){
+        return num%2==0;
     }
     public static void main(String[] args) {
-        int resultado = sumar(10,6);
-        System.out.println("El resultado es: "+resultado);
+        Scanner scanner = new Scanner(System.in);
+        int opc=0;
+        do {
+            System.out.println("1. Saludar");
+            System.out.println("2. Sumar");
+            System.out.println("3. Par impar");
+            System.out.println("4. Salir");
+            System.out.println("Ingrese una opcion.");
+            opc=scanner.nextInt();
+            switch (opc){
+                case 1:
+                    saludar();
+                    break;
+                case 2:
+                    System.out.println(sumar(1,19));
+                    break;
+                case 3:
+                    System.out.println(esPar(48));
+                    break;
+                case 4:
+                    System.out.println("Gracias por usar el sistema!");
+                    break;
+                default:
+                    System.out.println("No aplica esta opcion");
+                    break;
+            }
+        }while (opc!=4);
     }
 }
